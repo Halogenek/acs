caba271a5b3861f0ebd6b8794479c569e2e4f180
mokrzesa <m.okrzesa@dook.pro>
2020-10-22 14:10:49 +0200

    Initial commit with:   - project requirements,   - software initialization README,   - idea for dependency separation for easier testing.

--- src/software/com.py
+++ src/software/com.py
@@ -12,17 +12,43 @@ import serial.tools.list_ports
 FTDI_IC_VID_PID = "0403:6015"  # TODO change to (?) custom VID:PID
 
 
-class AcsCom:
-    """Serial interface (basic) communication class"""
-    serialPort = ''
+# TODO: ContextManager + Threading
+class SerialMediator:
+    """
+    Main Mediator responsibilities:
+      - open connection,
+      - close connection with care,
+      - read from serial,
+      - react on loosing connection,
+      - etc..
 
-    def __init__(self):
-        for comport in serial.tools.list_ports.comports():
-            if FTDI_IC_VID_PID in comport.hwid:
-                self.serialPort = comport.device
-        if self.serialPort == '':
-            raise ConnectionError("ACS: No PC interface card detected")
-        print(self.serialPort)
+        LOGIC MOVED TO MEDIATOR
+            for comport in serial.tools.list_ports.comports():
+                if FTDI_IC_VID_PID in comport.hwid:
+                    self.serialPort = comport.device
+            if self.serialPort == '':
+                raise ConnectionError("ACS: No PC interface card detected")
+            print(self.serialPort)
+
+    """
+
+    def __init__(self, vid_pid: str):
+        # vid_pid dependency - injection
+        pass
+
+
+# TODO: Make use of dependencies injected from SerialMediator
+class AcsCom:
+    """
+    Use Mediator for  clearer picture + dependencies separation.
+
+    AcsCom responsibilities:
+      - resolving incoming data,
+      - ...
+    """
+
+    def __init__(self, serial_mediator: SerialMediator):
+        pass
 
 
 test = AcsCom()
